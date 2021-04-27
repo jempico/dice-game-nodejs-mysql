6bc6087d01463cbb25bcea4772aa9401b159e708
Jemimah Pico <jpfilarca@gmail.com>
2021-04-27 15:41:06 +0200

    Update ranking.js

--- src/api/controllers/ranking.js
+++ src/api/controllers/ranking.js
@@ -5,8 +5,10 @@ class RankingController {
     async readPlayers(req, res) {
         try {
             const rankedPlayers = await ranking.getPlayers()
-            const totalAverage = await ranking.getTotalAverage()
-            res.status(200).json({ success: true, overall_successRate: totalAverage, ranking: rankedPlayers })
+            const totalAverageObj = await ranking.getTotalAverage()
+            let totalAverageNum = "";
+            totalAverageObj.forEach(obj => totalAverageNum = obj.total )
+            res.status(200).json({ success: true, overall_successRate: totalAverageNum, ranking: rankedPlayers })
         } catch(err) {
             res.status(400).json({ success: false, error: err})}
     }
